7bc5c76d57c3d56b3dd816652f7db1a866a49e6f
Rishika Machina <39758648+RishikaMachina@users.noreply.github.com>
2020-03-23 00:27:22 -0700

    Update Bisecting-K-means.py

--- Bisecting-K-means.py
+++ Bisecting-K-means.py
@@ -1,6 +1,34 @@
-import pandas as pd
-import numpy as np
-from scipy.sparse import csr_matrix
+class file_read():
+    def __init__(self):
+        pass
+    
+    def read_file(self):
+        with open("train.dat") as file:
+            line = file.readlines()
 
-with open('format.dat') as fn:
-    documents = fn.readlines()
+        rows = len(line)
+        cols = 0 
+        nonZeroCount = 0 
+        n = 0
+
+        for i in range(rows):
+            line_splitted = line[i].split()
+            nonZeroCount += len(line_splitted)/2
+            for j in range(0, len(line_splitted), 2): 
+                columval = int(line_splitted[j]) - 1
+                if columval + 1 > cols:
+                    cols = columval + 1
+        indptr = np.zeros(int(rows) + 1, dtype = np.float)
+        indices = np.zeros(int(nonZeroCount), dtype = np.int)
+        data = np.zeros(int(nonZeroCount), dtype = np.float)
+
+        for i in range(rows):
+            p = line[i].split()
+            for j in range(0, len(p), 2): 
+                indices[n] = int(p[j]) - 1
+                data[n] = float(p[j+1])
+                n += 1
+            indptr[i+1] = n 
+
+        return csr_matrix((data, indices, indptr), shape=(rows, cols), dtype = np.float)
+    
